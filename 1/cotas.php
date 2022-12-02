c698aabfdd4fd5bfbddb9b3aec9388ec51c7f0d7
rodri-afa <rodrigo.afaformacion@gmail.com>
2022-12-02 14:40:44 +0100

    correccion viernes

--- 1/cotas.php
+++ 1/cotas.php
@@ -1,1 +1,64 @@
 <?php
+
+$temperaturas = [
+    "lunes" => ["tmax" => 14],
+    "mnartes" => ["tmax" => 12],
+    "miercoles" => ["tmax" => 13],
+    "jueves" => ["tmax" => 13],
+    "viernes" => ["tmax" => 14],
+    "sabado" => ["tmax" => 15],
+    "domingo" => ["tmax" => 17],
+];
+
+// $temperaturas2= [
+//     [
+//         "dia"=>"lunes",
+//         "tmax"=>14
+//     ],
+//     [
+//         "dia"=>"lunes",
+//         "tmax"=>14
+//     ],
+//     [
+//         "dia"=>"lunes",
+//         "tmax"=>14
+//     ],
+//     [
+//         "dia"=>"lunes",
+//         "tmax"=>14
+//     ],
+//     [
+//         "dia"=>"lunes",
+//         "tmax"=>14
+//     ],
+//     [
+//         "dia"=>"lunes",
+//         "tmax"=>14
+//     ],
+//     [
+//         "dia"=>"lunes",
+//         "tmax"=>14
+//     ],
+//     [
+//         "dia"=>"lunes",
+//         "tmax"=>14
+//     ],
+// ]
+
+
+foreach ($temperaturas as $dia => $valores) {
+    $max = $valores["tmax"];
+    $min =  $max - rand(5, 10);
+    $temperaturas[$dia]["tmin"] = $min;
+    $temperaturas[$dia]["tmedia"] = ($max - $min) / 2;
+}
+
+// var_dump($temperaturas);
+
+$temperaturasJSON = json_encode($temperaturas, JSON_UNESCAPED_UNICODE);
+file_put_contents("./temperaturas.json", $temperaturasJSON);
+
+
+
+$temperaturasJSONLectura = file_get_contents("./temperaturas.json");
+var_dump($temperaturasJSONLectura);
